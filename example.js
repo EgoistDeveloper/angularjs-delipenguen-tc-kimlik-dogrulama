9ba2a77c220ff1035769d8fc0dcd554b58132043
Yıldıray Eyüp Erdoğan <epsilonelittr@gmail.com>
2021-03-27 10:00:09 +0300

    add only with directive example

--- example.js
+++ example.js
@@ -124,12 +124,17 @@ app.controller('exampleController', function exampleController($scope, $http) {
         message: null
     };
 
+    $scope.tcNo2 = {
+        isValid: true,
+        message: null
+    };
+
     /**
      * Generate random TC No
      * 
      * @source https://gist.github.com/canerbasaran/9440338
      */
-    $scope.tcNoGenerate = function tcNoGenerate() {
+    $scope.tcNoGenerate = function tcNoGenerate(model) {
         var tcno = '' + Math.floor(900000001 * Math.random() + 1e8),
             list = tcno.split('').map(function (t) {
                 return parseInt(t, 10)
@@ -138,6 +143,6 @@ app.controller('exampleController', function exampleController($scope, $http) {
             even = list[1] + list[3] + list[5] + list[7],
             tc10 = (7 * odd - even) % 10;
 
-        $scope.tcNo.text = tcno + ('' + tc10) + ('' + (even + odd + tc10) % 10);
+        model.text = tcno + ('' + tc10) + ('' + (even + odd + tc10) % 10);
     };
 });
